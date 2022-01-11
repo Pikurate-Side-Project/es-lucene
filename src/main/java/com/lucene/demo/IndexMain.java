aaa0c66bd33a672189078c1b58bfb927b5a38b06
jerry.jho <jerry.jho@pikurate.com>
2022-01-11 11:43:42 +0900

    feat: change documnet field

--- src/main/java/com/lucene/demo/IndexMain.java
+++ src/main/java/com/lucene/demo/IndexMain.java
@@ -2,13 +2,18 @@ package com.lucene.demo;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
@@ -21,20 +26,27 @@ public class IndexMain {
         
         File indexDirectory = new File("index");
         Directory directory = FSDirectory.open(indexDirectory.toPath());
-        IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(new StandardAnalyzer()));
+        Map<String, Analyzer> analyzerPerField = new HashMap<>();
+        analyzerPerField.put("name", new MyAnalyzer());
+        analyzerPerField.put("description", new MyAnalyzer());
+        PerFieldAnalyzerWrapper wrapper = new PerFieldAnalyzerWrapper(new StandardAnalyzer(), analyzerPerField);
+        IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(wrapper));
 
         List<Person> personList = Arrays.asList(
-            new Person("id0", "조재구", "나는 사람입니다.", 25),
-            new Person("id1", "우혜인", "나는 사람입니다.", 23),
-            new Person("id2", "조재혁", "나는 사람입니다.", 28)
+            new Person("id0", "jaegoo cho", "나는 안녕", 23),
+            new Person("id1", "jaegoo", "나는 안녕", 25),
+            new Person("id3", "goo jho", "나는 안녕", 28),
+            new Person("id4", "jaehyuk jho", "나는 안녕", 32),
+            new Person("id5", "hyein woo", "나는 안녕", 58),
+            new Person("id6", "cho", "나는 안녕", 23)
         );
         
         for (Person person : personList) {
-            Term term = new Term("ID", person.getId());
+            Term term = new Term("id", person.getId());
             Document document = new Document();
             document.add(new StringField("id", person.getId(), Field.Store.YES));
-            document.add(new StringField("name", person.getName(), Field.Store.YES));
-            document.add(new StringField("description", person.getDescription(), Field.Store.YES));
+            document.add(new TextField("name", person.getName(), Field.Store.YES));
+            document.add(new TextField("description", person.getDescription(), Field.Store.YES));
             document.add(new LongPoint("age", person.getAge()));
             indexWriter.updateDocument(term, document);
         }
