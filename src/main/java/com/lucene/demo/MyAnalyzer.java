c4f57a511ed83a6f84eaf19720238498d66f85d5
jerry.jho <jerry.jho@pikurate.com>
2022-01-11 11:45:47 +0900

    feat: search, analyze process

--- src/main/java/com/lucene/demo/MyAnalyzer.java
+++ src/main/java/com/lucene/demo/MyAnalyzer.java
@@ -3,14 +3,16 @@ package com.lucene.demo;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
-import org.apache.lucene.analysis.ngram.NGramTokenizer;
+import org.apache.lucene.analysis.ngram.NGramTokenFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 public class MyAnalyzer extends Analyzer {
 
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
-        NGramTokenizer src = new NGramTokenizer();
+        StandardTokenizer src = new StandardTokenizer();
         TokenStream result = new LowerCaseFilter(src);
+        result = new NGramTokenFilter(result, 1, 10, true);
         return new TokenStreamComponents(src, result);
     }
 }
--- src/main/java/com/lucene/demo/SearchMain.java
+++ src/main/java/com/lucene/demo/SearchMain.java
@@ -2,14 +2,19 @@ package com.lucene.demo;
 
 import java.io.File;
 
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.PrefixQuery;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
@@ -18,23 +23,27 @@ import org.apache.lucene.store.FSDirectory;
 public class SearchMain {
     
     public static void main(String[] args) throws Exception {
+        // QueryParser queryParser = new QueryParser("name", new WhitespaceAnalyzer());
+        // Query query = queryParser.parse("name:조재");
+
         File indexDirectory = new File("index");
         Directory directory = FSDirectory.open(indexDirectory.toPath());
         IndexSearcher indexSearcher = new IndexSearcher(DirectoryReader.open(directory));
+        Query query = new PrefixQuery(new Term("name", "jae"));
+        // Query query = queryParser.parse("jaegoo jho");
+        // Query ageQuery = LongPoint.newSetQuery("age", 25);
+        // BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
+        // booleanQueryBuilder.add(nameQuery, BooleanClause.Occur.SHOULD);
+        // booleanQueryBuilder.add(ageQuery, BooleanClause.Occur.SHOULD);
+        // TopDocs topDocs = indexSearcher.search(booleanQueryBuilder.build(), 10);
+        TopDocs topDocs = indexSearcher.search(query, 10);
 
-        TermQuery nameQuery = new TermQuery(new Term("name", "우혜인"));
-        Query ageQuery = LongPoint.newSetQuery("age", 25);
-
-        BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
-        booleanQueryBuilder.add(nameQuery, BooleanClause.Occur.MUST);
-        booleanQueryBuilder.add(ageQuery, BooleanClause.Occur.MUST);
-
-        TopDocs topDocs = indexSearcher.search(booleanQueryBuilder.build(), 10);
-        System.out.println("cout: " + topDocs.totalHits.value);
         long searchCount = topDocs.totalHits.value;
-        for (int index = 0; index < searchCount; index++) {
-            Document documnet = indexSearcher.doc(topDocs.scoreDocs[index].doc);
-            System.out.println("\t - id: " + documnet.get("id"));
+        System.out.println("cout: " + searchCount);
+
+        for (ScoreDoc scoreDoc: topDocs.scoreDocs) {
+            Document document = indexSearcher.doc(scoreDoc.doc);
+            System.out.println(" - name: " + document.get("name") + "(" + document.get("id") + ")");
         }
     }
 }
